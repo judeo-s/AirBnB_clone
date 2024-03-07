ca8b84ece0f49d60ccf6f6dd72595fa1e8a379d5
sw-ouahmane <133534816+sw-ouahmane@users.noreply.github.com>
2024-03-07 12:41:56 +0100

    Add

--- models/base_model.py
+++ models/base_model.py
@@ -1,48 +1,47 @@
 #!/usr/bin/python3
-
-'''A module used to act as a base for other modules'''
+""" Module that contains class BaseModel """
 
 from datetime import datetime
-import json
 from uuid import uuid4
 
 
 class BaseModel:
-    '''
-    A base class that defines all common attributes/methods for other classes
-    '''
+    """Base class"""
 
-    def __init__(self):
-        '''A constructor function for the BaseModel class'''
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **my_dict):
+        """ constructor """
+        if my_dict:
+            for key, value in my_dict.items():
+                if key == "created_at" or key == "updated_at":
+                    t = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, t)
+                elif key == "__class__":
+                    continue
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id: str = str(uuid4())
+            self.created_at: datetime = datetime.now()
+            self.updated_at: datetime = self.created_at
+            # from models import storage
+            # storage.new(self)
+            # storage.save()
 
-    def __str__(self):
-        '''
-        An method that returns a custom string representing the class
+    def __str__(self) -> str:
+        """to string"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
-        Returns:
-            str
-        '''
-        return f'[{BaseModel.__name__}] ({self.id}) {self.__dict__}'
-
-    def save(self):
-        '''
-        A method that updates the updated_at with the current datetime
-        '''
+    def save(self) -> None:
+        """ update """
         self.updated_at = datetime.now()
+        from models import storage
+        storage.new(self)
+        storage.save()
 
-    def to_dict(self):
-        '''
-        A function that returns a dictionary containing all key/values of
-        __dict__ of the instance
-
-        Return:
-            dict
-        '''
-        class_dict = {}
-        class_dict.update(self.__dict__)
-        class_dict['created_at'] = class_dict['created_at'].isoformat()
-        class_dict['updated_at'] = class_dict['updated_at'].isoformat()
-        return class_dict
+    def to_dict(self) -> dict:
+        """return dic"""
+        my_dict = self.__dict__.copy()
+        my_dict.update({"__class__": self.__class__.__name__})
+        my_dict.update({"created_at": str(self.created_at.isoformat())})
+        my_dict.update({"updated_at": str(self.updated_at.isoformat())})
+        return my_dict
