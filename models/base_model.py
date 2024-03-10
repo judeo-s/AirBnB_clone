1e3b86e135a0e65da31d25579dcb77cf3dacb9e1
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-03-10 07:30:19 +0000

    made changes to a few functions in the base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ class BaseModel:
     """Base class"""
 
     def __init__(self, *args, **my_dict):
-        """ constructor """
+        """ constructor for BaseModel class """
         if my_dict:
             for key, value in my_dict.items():
                 if key == "created_at" or key == "updated_at":
@@ -23,23 +23,20 @@ class BaseModel:
             self.id: str = str(uuid4())
             self.created_at: datetime = datetime.now()
             self.updated_at: datetime = self.created_at
-            # from models import storage
-            # storage.new(self)
-            # storage.save()
 
     def __str__(self) -> str:
-        """to string"""
+        """ string representation of BaseModel class """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self) -> None:
-        """ update """
+        """ updates the self.updated_at """
         self.updated_at = datetime.now()
         from models import storage
         storage.new(self)
         storage.save()
 
     def to_dict(self) -> dict:
-        """return dic"""
+        """ returns a dict representation of instance"""
         my_dict = self.__dict__.copy()
         my_dict.update({"__class__": self.__class__.__name__})
         my_dict.update({"created_at": str(self.created_at.isoformat())})
